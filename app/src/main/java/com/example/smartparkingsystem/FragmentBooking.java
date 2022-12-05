8d8f05db175255fb7750754e690bdee3a2583f84
firzanahuda <hudafirzana@gmail.com>
2022-12-05 23:20:42 +0800

    Third

--- app/src/main/java/com/example/smartparkingsystem/FragmentBooking.java
+++ app/src/main/java/com/example/smartparkingsystem/FragmentBooking.java
@@ -2,18 +2,23 @@ package com.example.smartparkingsystem;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link FragmentBooking#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FragmentBooking extends Fragment {
+public class FragmentBooking extends Fragment implements OnMapReadyCallback {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -53,12 +58,19 @@ public class FragmentBooking extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_booking, container, false);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
     }
 }
